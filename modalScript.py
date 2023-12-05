70309fb9e8485eb16a423c970c8f84f1b3d0d029
Rinkal Khatrani <rinkalkhatrani123@gmail.com>
2023-12-05 13:47:36 +0530

    save

--- modalScript.py
+++ modalScript.py
@@ -1,23 +1,43 @@
-from transformers import AutoTokenizer, LlamaForCausalLM
+import logging
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# Install sentencepiece
-# You can run this command in your terminal or command prompt.
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Install necessary libraries
+# You can run these commands in your terminal or command prompt.
 # pip install sentencepiece
+# pip install transformers
 
-# tokenizer = AutoTokenizer.from_pretrained(
-#     "NEU-HAI/mental-alpaca", legacy=False)
-# model = AutoModelForCausalLM.from_pretrained("NEU-HAI/mental-alpaca")
+# Function to run the script
 
-model = LlamaForCausalLM.from_pretrained("NEU-HAI/mental-alpaca")
-tokenizer = AutoTokenizer.from_pretrained("NEU-HAI/mental-alpaca")
 
-prompt = "Hey, are you conscious? Can you talk to me?"
-inputs = tokenizer(prompt, return_tensors="pt")
+def run_script():
+    try:
+        logging.debug("Initializing tokenizer and model.")
+        tokenizer = AutoTokenizer.from_pretrained(
+            "NEU-HAI/mental-alpaca", legacy=False)
+        model = AutoModelForCausalLM.from_pretrained("NEU-HAI/mental-alpaca")
 
-prompt = "Hey, are you conscious? Can you talk to me?"
-inputs = tokenizer(prompt, return_tensors="pt")
-# generate_ids = model.generate(inputs.input_ids, max_length=30)
-# response = tokenizer.batch_decode(
-#     generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        prompt = "Hey, are you conscious? Can you talk to me?"
+        inputs = tokenizer(prompt, return_tensors="pt")
+        logging.debug("Generating text.")
+        generate_ids = model.generate(inputs.input_ids, max_length=30)
+        response = tokenizer.batch_decode(
+            generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 
-print(inputs)
+        logging.debug("Generated response: %s", response)
+
+    except Exception as e:
+        logging.error("An error occurred: %s", str(e))
+        raise  # Re-raise the exception for detailed traceback in logs
+
+
+if __name__ == "__main__":
+    try:
+        logging.debug("Script started.")
+        run_script()
+        logging.debug("Script finished.")
+
+    except Exception as e:
+        logging.exception("Script terminated with an exception: %s", str(e))
