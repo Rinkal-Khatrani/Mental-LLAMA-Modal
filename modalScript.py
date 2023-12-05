f8c1bdc140573ee89fe4ff355467d048b5dc654e
Rinkal Khatrani <rinkalkhatrani123@gmail.com>
2023-12-05 13:42:50 +0530

    script

--- modalScript.py
+++ modalScript.py
@@ -10,8 +10,8 @@ model = AutoModelForCausalLM.from_pretrained("NEU-HAI/mental-alpaca")
 
 prompt = "Hey, are you conscious? Can you talk to me?"
 inputs = tokenizer(prompt, return_tensors="pt")
-generate_ids = model.generate(inputs.input_ids, max_length=30)
-response = tokenizer.batch_decode(
-    generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+# generate_ids = model.generate(inputs.input_ids, max_length=30)
+# response = tokenizer.batch_decode(
+#     generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 
-print(response)
+print(inputs)
