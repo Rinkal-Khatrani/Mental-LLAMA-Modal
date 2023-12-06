4b159326975da22096d7d4841dea51f0276ffcee
Rinkal Khatrani <rinkalkhatrani123@gmail.com>
2023-12-06 15:36:41 +0530

    llama-chat script

--- LLAMAChat.py
+++ LLAMAChat.py
@@ -35,7 +35,7 @@ Therapist is giving instruction to the child. "Touch Banana"
 "'''
 
 prompt3 = '''Therapist instructions such as DO this and Follow me form in which domain or verbal operant of ABA'''
-inputs = tokenizer(prompt, return_tensors="pt")
+inputs = tokenizer(prompt2, return_tensors="pt")
 
 # Generate
 generate_ids = model.generate(inputs.input_ids, max_length=len(prompt2))
