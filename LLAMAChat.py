3cdacca38540c706ab60e46640cd170770136ed2
Rinkal Khatrani <rinkalkhatrani123@gmail.com>
2023-12-06 18:16:23 +0530

    script

--- LLAMAChat.py
+++ LLAMAChat.py
@@ -13,17 +13,15 @@ while True:
     input_text = input("You: ")
 
     if len(input_text) > 0:
-        encoded_input = tokenizer(input_text, return_tensors="pt")
-        with torch.no_grad():
-            outputs = model(**encoded_input)
-        logits = outputs.logits
+        # Tokenize the input
+        inputs = tokenizer(input_text, return_tensors="pt")
+        generate_ids = model.generate(
+            inputs.input_ids, max_length=len(input_text))
+        generated_text = tokenizer.batch_decode(
+            generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 
-        mask_token_index = (encoded_input.input_ids == tokenizer.mask_token_id)[
-            0].nonzero(as_tuple=True)[0]
-        predicted_token_id = logits[0, mask_token_index].argmax(axis=-1)
-        aa = tokenizer.decode(predicted_token_id)
-
-        print("Result: ", aa)
+        # Print the result
+        print("Bot:", generated_text)
 
     # Ask if the user wants to continue
     user_response = input("Do you want to continue? (yes/no): ").lower()
