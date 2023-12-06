bdeb4032e85005c3c7bd59783d1f986235704cca
Rinkal Khatrani <rinkalkhatrani123@gmail.com>
2023-12-06 15:33:47 +0530

    llama-chat script

--- LLAMAChat.py
+++ LLAMAChat.py
@@ -9,10 +9,36 @@ model = AutoModelForCausalLM.from_pretrained(
 
 prompt = '''Analyze this instruction in an ABA session and tell me what is it Token economy
 "Now, so we're going to be using this talking board. And when you reach the goal, then you're going to get the puzzle."'''
+prompt2 = '''For an ABA session where a child is undergoing therapy with a therapist , analyze the conversation  sequentially  between child and therapist and act as a Board Certified Behavior Analyst and do the following. 
+1. Take only those conversations where therapist is giving instruction to the child and Identify which verbal operants are present in those instructions from the list below. 
+2. For each conversation set identify if the targets were done correctly by the child.  Usually targets are correctly done if therapist appreciates the child. Also list targets that were not done correctly by the child. List which target was done how many time, and child responded how many time correctly and incorrectly. 
+3. What are the area of improvements for a therapist?
+4. Generate a summary in the end. 
+
+- Mands, Spontaneous Verbalizations
+- Tacting
+- Intraverbals
+- Listener Response / Receptive Language
+-  Echoics
+-  Imitation
+-  Gross Motor Skills
+-  Fine Motor Skills
+-  Visual Perceptual and Matching to Sample
+-  Disruptive Behaviors
+-  Reinforcement and Praise 
+
+This is a ABA therapy session text: 
+Therapist is making the child ready for the session: OK, ready?
+Therapist is giving instruction to the child. "Touch Banana"
+Therapist is responding to child's response : No, try again.
+Therapist is giving instruction to the child. "Touch Banana"
+"'''
+
+prompt3 = '''Therapist instructions such as DO this and Follow me form in which domain or verbal operant of ABA'''
 inputs = tokenizer(prompt, return_tensors="pt")
 
 # Generate
-generate_ids = model.generate(inputs.input_ids, max_length=len(prompt))
+generate_ids = model.generate(inputs.input_ids, max_length=len(prompt2))
 aa = tokenizer.batch_decode(
     generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 print("Response", aa)
